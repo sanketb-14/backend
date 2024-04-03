27e42c67e66f85048200af5448918f4dbe5b0061
sanket_barapatre <sanketbarapatre999@gmail.com>
2024-04-03 16:23:52 +0530

    making changes in server.js file

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200 , {
-        contentType: 'text/plain'
-    })
-    res.end("Hello sanket")
+    const { url } = req;
+
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome Sanket');
+
+    }
+    else if(url === '/home'){
+        res.writeHead(200, { 'Content-Type': 'text/plain'})
+        res.end("Welcome Home")
+    }
+    else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to About Us page');
+    } else if (url === '/node') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to my Node Js project');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 - Not Found');
+    }
 })
 
 server.listen(4000 , () => {
