d6ca6ef610575fc8df9bbb4fa1156f5e8fe8adea
sanket_barapatre <sanketbarapatre999@gmail.com>
2024-04-04 13:11:22 +0530

    adding readFileServer.js file

---
+++ readFileServer.js
@@ -0,0 +1,56 @@
+
+const http = require('http');
+const fs = require('fs')
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+    const filePath = path.join(__dirname, "message.txt")
+    if (url === '/') {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            let formValue = data || "undefined"
+            if (err) {
+                console.error(err)
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.write(`
+  <html>
+    <head><title>Enter Message</title></head>
+    <body>
+      <p>${formValue}</p>
+      <form action="/message" method="POST">
+        <input type="text" name="message" value="${formValue}">
+        <button type="submit">Send</button>
+      </form>
+    </body>
+  </html>
+`);
+        })
+
+    }
+    if(url === "/message" && method === "POST"){
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile(filePath, message, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.write('Internal Server Error');
+                    return res.end();
+                } else {
+                    res.writeHead(302, { 'Location': '/' });
+                    return res.end();
+                }
+            });
+
+        })
+    }
+})
+
+server.listen(5000, () => console.log('server is listening'))
